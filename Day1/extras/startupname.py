7a1b61637e1ab0f006b2cf71abe3318e7fc55bc4
bettercoder <abettercode@gmail.com>
2022-01-06 04:56:29 +0530

    made corrections in day1 extras

--- Day1/extras/startupname.py
+++ Day1/extras/startupname.py
@@ -1,4 +1,6 @@
+# Start up Name Generator
+
 print("Welcome to the StartUp Name Generator\n")
 country = input("In which country were you born?\n")
 animal = input("What is your favorite animal?\n")
-print("Your Band name could be " + country + " " + animal)
+print("Your StartUp name could be " + country + " " + animal + "s")
