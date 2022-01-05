c34a5fb461ded56a348a9ea8da318db64c21d9d9
bettercoder <abettercode@gmail.com>
2022-01-05 05:32:13 +0530

    Day 1 revised

--- Day1/bandname.py
+++ Day1/bandname.py
@@ -1,3 +1,5 @@
+# Printing,Commenting, Debugging, String Manipulation and Variables.
+
 print("Welcome to the Band Name Generator\n")
 city = input("Which city did you grow up in?\n")
 pet = input("What is the name of your pet?\n")
