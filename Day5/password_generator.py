8d682eb4da2f2c616fcd62ac510b60e84da88626
bettercoder <abettercode@gmail.com>
2022-01-09 23:11:46 +0530

    Day 5 code needs debugging

--- Day5/password_generator.py
+++ Day5/password_generator.py
@@ -15,28 +15,27 @@ nr_of_numbers = int(input("How many numbers do you want in your password? \n"))
 nr_of_symbols = int(input("How many symbols do you want in your password? \n"))
 
 # Easy Level
-password = ""
-for char in range(1, nr_of_letters + 1):
-    password += random.choice(letter)
-for nr in range(1, nr_of_numbers + 1):
-    password += random.choice(number)
-for sym in range(1, nr_of_symbols + 1):
-    password += random.choice(symbol)
-print(password)
+#password = ""
+# for char in range(1, nr_of_letters + 1):
+#    password += random.choice(letter)
+# for nr in range(1, nr_of_numbers + 1):
+#    password += random.choice(number)
+# for sym in range(1, nr_of_symbols + 1):
+#    password += random.choice(symbol)
+# print(password)
 
 # Hard Level
-# passwordlist = []
-#for char in range(1,nr_of_letters+1):
-#    password_list += random.choice(letter)
-#    for nr in range(1,nr_of_numbers+1):
-#        password_list += random.choice(number)
-#        for symb in range(1,nr_of_symbols+1):
-#            password_list += random.choice(symbol)
-#random.shuffle(password_list)
-#print(password_list)
+passwordlist = []
+for char in range(1, nr_of_letters+1):
+    password_list += random.choice(letter)
+    for nr in range(1, nr_of_numbers+1):
+        password_list += random.choice(number)
+        for symb in range(1, nr_of_symbols+1):
+            password_list += random.choice(symbol)
+random.shuffle(password_list)
+print(password_list)
 
-#password_hard = ""
-#for char in password_list:
-#    password_hard += char
-#print(f"Your password is {password_hard}")
-
+password_hard = ""
+for char in password_list:
+    password_hard += char
+print(f"Your password is {password_hard}")
