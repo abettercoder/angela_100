0cb185b20c677aeb0695b0052933433cff5e47ce
bettercoder <abettercode@gmail.com>
2022-01-16 03:44:19 +0530

    challenge4 random walk completed

--- Day18/challenge4_random-walk.py
+++ Day18/challenge4_random-walk.py
@@ -1,1 +1,19 @@
 # Generating a random walk in turtle.
+from turtle import Turtle, Screen
+import random
+
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed",
+           "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+tim = Turtle()
+
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+
+for _ in range(400):
+    tim.color(random.choice(colours))
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+
+screen = Screen()
+screen.exitonclick()
