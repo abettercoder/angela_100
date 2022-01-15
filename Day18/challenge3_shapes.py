b01b1ae5e4e3b6149278f75c653ebdee1399a675
abettercoder <abettercode@gmail.com>
2022-01-15 14:30:20 +0530

    challenge3 completed

--- Day18/challenge3_shapes.py
+++ Day18/challenge3_shapes.py
@@ -1,12 +1,21 @@
 from turtle import Turtle, Screen
+import random
 
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed",
+           "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 tim = Turtle()
 
-for _ in range(40):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+
+def draw_shape(num_sides):
+    angle = 360/(num_sides)
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colours))
+    draw_shape(shape_side_n)
 
 screen = Screen()
 screen.exitonclick()
