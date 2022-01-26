cb1fc0ae349e19f47fb9d2f9ce78fe35be9ae66b
bettercoder <abettercode@gmail.com>
2022-01-26 19:51:07 +0530

    Day25 US States challenge

--- Day25/us-states/main.py
+++ Day25/us-states/main.py
@@ -1,11 +1,13 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title = ("U.S. States Game")
-
 image = "blank_states_img.gif"
 screen.addshape(image)
-
 turtle.shape(image)
 
-screen.exitonclick()
+answer_state = screen.textinput(
+    title="Guess the State", prompt="What's another state's name?")
+
+states = pandas.read_csv("50")
